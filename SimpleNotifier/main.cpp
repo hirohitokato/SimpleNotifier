692f294838547a0d1bbeac15795aacb13f01d530
Hirohito Kato <hkato193@gmail.com>
2023-04-09 20:46:57 +0900

    Initial Commit

---
+++ SimpleNotifier/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleNotifier
+//
+//  Created by 加藤寛人 on 2023/04/09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
