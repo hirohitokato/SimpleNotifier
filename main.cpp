11882598d210e5606fe2d2cfbd96ccb11cb033fd
Hirohito Kato <hkato193@gmail.com>
2023-04-16 00:12:01 +0900

    Update sample program.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ static const auto VoidNotification = Notification<void>("Non Argument");
 
 Notifier sharedNotifier;
 
+
 int main(int argc, const char * argv[]) {
     {
         Notifier notifier;
@@ -22,21 +23,28 @@ int main(int argc, const char * argv[]) {
         notifier.notify(HelloNotification, 1);
         auto token1 = notifier.add_observer(HelloNotification,
                              std::function([&](int a){
-                                 printf("(1) called: %d\n",a);
+                                 printf("(1-1) called: %d\n",a);
                              }));
         auto token2 = notifier.add_observer(HelloNotification,
                              std::function([&](int a){
-                                 printf("(2) called: %d\n",a);
+                                 printf("(1-2) called: %d\n",a);
                              }));
-
         notifier.notify(HelloNotification, 1);
         std::cout << "----------------------\n";
-//        delete token1;
-        notifier.notify(HelloNotification, 2);
-        std::cout << "----------------------\n";
-        notifier.remove_observer(token2);
+        {
+            auto token = notifier.add_observer(HelloNotification,
+                                 std::function([&](int a){
+                                     printf("(2-1) called: %d\n",a);
+                                 }));
+            notifier.notify(HelloNotification, 2);
+            delete token;
+        }
         notifier.notify(HelloNotification, 3);
         std::cout << "----------------------\n";
+        notifier.remove_observer(token2);
+        notifier.notify(HelloNotification, 4);
+        std::cout << "----------------------\n";
+
 
         notifier.add_observer(VoidNotification, std::function<void()>([&](){
             printf("called\n");
